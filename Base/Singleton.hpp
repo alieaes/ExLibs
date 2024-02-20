28e0726aa085d866446410948c185b1c1fe96ff0
hogyun.yoon <alieaes54@gmail.com>
2024-02-20 18:50:35 +0900

    Add : Singleton

---
+++ Base/Singleton.hpp
@@ -0,0 +1,55 @@
+﻿#ifndef __HDR_EXT_SINGLETON__
+#define __HDR_EXT_SINGLETON__
+
+#include "Base/ExtConfig.hpp"
+
+#include <mutex>
+
+namespace Ext
+{
+    namespace Singletons
+    {
+        template < typename T >
+        class Singleton
+        {
+        public:
+
+            static T* GetInstance()
+            {
+                if( _instance == NULLPTR )
+                    std::call_once( _flag, []() { _instance = new T; } );
+
+                return _instance;
+            }
+
+            static void DestroyInstance()
+            {
+                if( _instance != NULLPTR )
+                {
+                    delete _instance;
+                    _instance = NULLPTR;
+                }
+            }
+
+        private:
+            Singleton() {}
+            ~Singleton() {}
+
+            Singleton( const Singleton& ) = delete;
+            Singleton( Singleton&& ) = delete;
+            Singleton& operator=( const Singleton& ) = delete;
+            Singleton& operator=( Singleton&& ) = delete;
+
+            static T* _instance;
+            static std::once_flag _flag;
+        };
+
+        template < typename T >
+        T* Singleton<T>::_instance = NULLPTR;
+
+        template < typename T >
+        std::once_flag Singleton<T>::_flag;
+    }
+}
+
+#endif
