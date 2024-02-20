299b7b12576bdef5d585d56470c50b4b8f782596
hogyun.yoon <alieaes54@gmail.com>
2024-02-20 18:51:10 +0900

    Add : Config file used as default

---
+++ Base/ExtConfig.hpp
@@ -0,0 +1,10 @@
+#ifndef __HDR_EXT_BASE_CONFIG__
+#define __HDR_EXT_BASE_CONFIG__
+
+#define NULLPTR nullptr
+
+#define DEFAULT_TIME_STRING "yyyy-MM-dd HH:mm:ss"
+
+#define PORT_RANGE_MAX 65535
+
+#endif
