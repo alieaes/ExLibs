d18fb63a3441e3bf5631be6e9ab131758ad44419
hogyun.yoon <alieaes54@gmail.com>
2024-03-10 00:00:25 +0900

    Add : XString, String Class used inside the library

---
+++ String/EXString.cpp
@@ -0,0 +1,277 @@
+﻿#include "stdafx.h"
+#include "EXString.hpp"
+
+#include <algorithm>
+#include <codecvt>
+#include <Windows.h>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+#pragma warning(disable:4996)
+
+std::string XString::toString() const
+{
+    using convert_type = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_type, wchar_t> cvt;
+
+    auto tmp = cvt.to_bytes( _str );
+    return tmp;
+}
+
+std::wstring XString::toWString() const
+{
+    return _str;
+}
+
+#ifdef QSTRING_H
+QString XString::toQString() const
+{
+    return QString::fromStdWString( _str );
+}
+#endif
+
+bool XString::toBool() const
+{
+    if( auto tmp = toLower(); tmp == L"y" || tmp == L"true" || tmp == L"1" || tmp == L"yes" )
+        return true;
+
+    return false;
+}
+
+int XString::toInt() const
+{
+    int n = 0;
+    try
+    {
+        n= std::stoi( _str );
+    }
+    catch( [[maybe_unused]] const std::invalid_argument& ia )
+    {
+    }
+
+    return n;
+}
+
+long XString::toLong() const
+{
+    return std::stol( _str );
+}
+
+XString XString::toLower() const
+{
+    std::wstring tmp = _str;
+    std::ranges::transform( tmp, tmp.begin(), towlower );
+
+    return tmp;
+}
+
+XString XString::toUpper() const
+{
+    std::wstring tmp = _str;
+    std::ranges::transform( tmp, tmp.begin(), towupper );
+
+    return tmp;
+}
+
+std::vector<char> XString::toCharByte() const
+{
+    std::wstring tmp = _str;
+
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+    std::string string = converter.to_bytes( tmp );
+    std::vector< char > ret( string.begin(), string.end() );
+
+    return ret;
+}
+
+bool XString::IsEmpty() const
+{
+    return _str.empty();
+}
+
+bool XString::IsDigit() const
+{
+    bool isDigit = false;
+    int n = toInt();
+
+    do
+    {
+        if( n == 0 )
+        {
+            if( compare( "0" ) == 0 )
+            {
+                isDigit = true;
+                break;
+            }
+
+            isDigit = false;
+        }
+        else
+        {
+            isDigit = true;
+        }
+    }
+    while( false );
+
+    return isDigit;
+}
+
+bool XString::Endl() const
+{
+    return _nPos > size() ? true : false;
+}
+
+size_t XString::size() const
+{
+    return _str.size();
+}
+
+int XString::count( const XString& find ) const
+{
+    int nCount = 0;
+
+    if( find.IsEmpty() == true )
+        return nCount;
+
+    size_t sizeFind = std::string::npos;
+    size_t idx = 0;
+
+    while( true )
+    {
+        sizeFind = _str.find( find.toWString(), idx );
+        if( sizeFind != std::string::npos )
+        {
+            nCount++;
+            idx = sizeFind + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return nCount;
+}
+
+XString XString::substr( size_t nDst ) const
+{
+    if( nDst == std::string::npos )
+        return "";
+
+    return _str.substr( nDst );
+}
+
+XString XString::substr( size_t nSrc, size_t nSize ) const
+{
+    if( nSrc == std::string::npos || nSize == std::string::npos )
+        return "";
+
+    return _str.substr( nSrc, nSize );
+}
+
+size_t XString::find_last_of( XString xs ) const
+{
+    return _str.find_last_of( xs.toWString() );
+}
+
+size_t XString::find( XString xs ) const
+{
+    return _str.find( xs.toWString() );
+}
+
+const wchar_t* XString::c_str() const
+{
+    return _str.c_str();
+}
+
+std::vector<XString> XString::split( const XString& sSplit ) const
+{
+    int nCnt = count( sSplit );
+    size_t nSize = sSplit.size();
+
+    std::vector<XString> vecList;
+    vecList.reserve( nCnt );
+
+    std::string::size_type st = _str.find( sSplit.toWString() );
+    std::string::size_type nCurrent = 0;
+
+    while( st != std::string::npos )
+    {
+        vecList.push_back( _str.substr( nCurrent, st - nCurrent ) );
+
+        nCurrent = st + nSize;
+        st = _str.find( sSplit.toWString(), st + nSize );
+    }
+
+    return vecList;
+}
+
+XString XString::replaceAll( const XString& xa, const XString& xb ) const
+{
+    std::wstring tmp = _str;
+
+    for( size_t idx = tmp.find( xa.toWString() ); idx >= 0; idx = tmp.find( xa.toWString() ) )
+    {
+        if( idx == std::string::npos )
+            break;
+
+        tmp.replace( idx, xa.size(), xb.toWString() );
+    }
+
+    return tmp;
+}
+
+XString XString::replace( const XString& xa, const XString& xb ) const
+{
+    std::wstring tmp = _str;
+    tmp.replace( tmp.find( xa.toWString() ), xa.size(), xb.toWString() );
+
+    return tmp;
+}
+
+int XString::compare( const XString& xs, bool isCaseInsensitive /*= false*/ ) const
+{
+    if( isCaseInsensitive == true )
+    {
+        const std::wstring xa{ toLower() };
+        const std::wstring xb{ xs.toLower() };
+
+        return xa.compare( xb );
+    }
+    else
+    {
+        return _str.compare( xs.toWString() );
+    }
+}
+
+bool XString::contains( const XString& xs, bool isCaseInsensitive /*= false*/ ) const
+{
+    if( isCaseInsensitive == true )
+    {
+        const std::wstring tmp{ toLower() };
+        return tmp.find( xs.toLower() ) != std::string::npos;
+    }
+    return _str.find( xs.toLower() ) != std::string::npos;
+}
+
+bool XString::startswith( const XString& xs, bool isCaseInsensitive /*= false*/ ) const
+{
+    if( isCaseInsensitive == true )
+    {
+        const std::wstring tmp{ toLower() };
+        return tmp.starts_with( xs.toLower() );
+    }
+    return _str.starts_with( xs );
+}
+
+bool XString::endswith( const XString& xs, bool isCaseInsensitive /*= false*/ ) const
+{
+    if( isCaseInsensitive == true )
+    {
+        const std::wstring tmp{ toLower() };
+        return tmp.ends_with( xs.toLower() );
+    }
+    return _str.ends_with( xs );
+}
---
+++ String/EXString.hpp
@@ -0,0 +1,346 @@
+﻿#ifndef __HDR_EXT_XSTRING__
+#define __HDR_EXT_XSTRING__
+
+#define EXT_USING_XSTRING
+
+#include <codecvt>
+#include <sstream>
+#include <vector>
+
+#include "Windows.h"
+
+class XString
+{
+public:
+    XString()
+    {
+        _str = std::wstring();
+    }
+
+    XString( const char* c )
+    {
+        if( c == NULL )
+            return;
+
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
+        _str = converter.from_bytes( c );
+    }
+
+    XString( const wchar_t* wc )
+    {
+        if( wc == NULL )
+            return;
+
+        _str = wc;
+    }
+
+    XString( const unsigned char* c )
+    {
+        if( c == NULL )
+            return;
+
+        std::string s = static_cast< std::string >( reinterpret_cast< const char* >( c ) ); // new style
+        _str = std::wstring( s.begin(), s.end() );
+    }
+
+    XString( DWORD dw )
+    {
+        _str = std::to_wstring( dw );
+    }
+
+    XString( int n )
+    {
+        _str = std::to_wstring( n );
+    }
+
+    XString( unsigned int n )
+    {
+        _str = std::to_wstring( n );
+    }
+
+    XString( long n )
+    {
+        _str = std::to_wstring( n );
+    }
+
+    XString( ULONGLONG ull )
+    {
+        _str = std::to_wstring( ull );
+    }
+
+    XString( double d )
+    {
+        _str = std::to_wstring( d );
+    }
+
+    XString( const std::wstring& ws )
+    {
+        _str = ws;
+    }
+
+    XString( const std::string& s )
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
+        _str = converter.from_bytes( s );
+    }
+
+    XString( const std::wstringstream& ws )
+    {
+        _str = ws.rdbuf()->str();
+    }
+
+#ifdef QSTRING_H
+    XString( const QString& qs )
+    {
+        _str = qs.toStdWString();
+    }
+#endif // QSTRING_H
+
+    ~XString()
+    = default;
+
+    auto operator<=>( XString const& ) const = default;
+
+    /////////////// XString <- X ///////////////
+    XString operator = ( std::wstring& ws )          // XString xs = std::wstring( L"TEST" );
+    {
+        _str = ws;
+        return _str;
+    }
+
+    XString operator = ( std::string& s )            // XString xs = std::string( "TEST" );
+    {
+        _str = std::wstring( s.begin(), s.end() );
+        return _str;
+    }
+
+    XString operator = ( const char* c )             // XString xs = "TEST";
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        _str = converter.from_bytes( c );
+
+        return _str;
+    }
+
+    XString operator = ( const unsigned char* c )             // XString xs = "TEST";
+    {
+        std::string s = static_cast< std::string >( reinterpret_cast< const char* >( c ) ); // new style
+
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        _str = converter.from_bytes( s );
+
+        if( _str.empty() == false )
+            _str.pop_back();
+
+        return _str;
+    }
+
+    XString operator = ( const wchar_t* wc )         // XString xs = L"TEST";
+    {
+        _str = wc;
+        return _str;
+    }
+
+    XString operator = ( const XString& wc )         // XString xs = L"TEST";
+    {
+        _str = wc.toWString();
+        return _str;
+    }
+
+    XString operator = ( const std::wstringstream& ws )
+    {
+        _str = ws.rdbuf()->str();
+        return _str;
+    }
+
+#ifdef QSTRING_H
+    XString operator = ( QString& qs )               // XString xs = QString( "TEST" );
+    {
+        _str = qs.toStdWString();
+        return _str;
+    }
+#endif  // QSTRING_H
+    ////////////////////////////////////////////
+
+    /////////////// XString -> X ///////////////
+    operator std::string() const
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        return converter.to_bytes( _str );
+    }
+    //operator std::wstring() const { return _str; }
+
+    operator LPCWSTR() const { return _str.c_str(); }
+
+#ifdef USING_QT_LIBS
+    operator QString() const { return QString::fromStdWString( _str ); }
+#endif  // USING_QT_LIBS
+    ////////////////////////////////////////////
+
+    XString operator+= ( int n )
+    {
+        _str += std::to_wstring( n );
+        return _str;
+    }
+
+    XString operator+= ( double d )
+    {
+        _str += std::to_wstring( d );
+        return _str;
+    }
+
+    XString operator+= ( XString xs )
+    {
+        _str += xs.string();
+        return _str;
+    }
+
+    XString operator +=( char c )
+    {
+        _str += std::to_wstring( c );
+        return _str;
+    }
+
+    XString operator +=( const char* c )
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        _str += converter.from_bytes( c );
+        return _str;
+    }
+
+    XString operator += ( std::string& s )
+    {
+        _str += std::wstring( s.begin(), s.end() );
+        return _str;
+    }
+
+    XString operator +( std::wstring& xs )
+    {
+        _str += xs;
+        return _str;
+    }
+
+    XString operator +( const char* c )
+    {
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        _str += converter.from_bytes( c );
+        return _str;
+    }
+
+    XString operator +( const std::wstring& xs )
+    {
+        _str += xs;
+        return _str;
+    }
+
+    bool operator ==( std::wstring& xs ) const
+    {
+        return xs.compare( _str ) == 0 ? true : false;
+    }
+
+    bool operator ==( const XString& xs ) const
+    {
+        return _str.compare( xs.toWString() ) == 0 ? true : false;
+    }
+
+    bool operator ==( XString& xs ) const
+    {
+        return _str.compare( xs.toWString() ) == 0 ? true : false;
+    }
+
+    bool operator ==( const std::wstring& xs ) const
+    {
+        return xs.compare( _str ) == 0 ? true : false;
+    }
+
+    bool operator ==( const wchar_t* wc ) const
+    {
+        return _str.compare( wc ) == 0 ? true : false;
+    }
+
+    XString operator *() const
+    {
+        if( _nPos > size() )
+            return NULL;
+        return _str[ _nPos ];
+    }
+
+    XString operator ++()
+    {
+        return _str[ _nPos++ ];
+    }
+
+    XString operator ++( int )
+    {
+        if( _nPos + 1 > size() )
+        {
+            ++_nPos;
+            return NULL;
+        }
+        return _str[ ++_nPos ];
+    }
+
+    XString operator --( int )
+    {
+        if( _nPos - 1 < size() )
+        {
+            --_nPos;
+            return NULL;
+        }
+        return _str[ --_nPos ];
+    }
+
+    std::string                      toString() const;
+    std::wstring                     toWString() const;
+#ifdef QSTRING_H
+    QString                          toQString() const;
+#endif
+    // 값이 존재한다고 TRUE가 되지 않음, 1/Yes/True/y 의 값이여야 TRUE로 반환됨
+    bool                             toBool() const;
+    int                              toInt() const;
+    long                             toLong() const;
+
+    XString                          toLower() const;
+    XString                          toUpper() const;
+
+    std::vector< char >              toCharByte() const;
+
+    bool                             IsEmpty() const;
+    bool                             IsDigit() const;
+    bool                             Endl() const;
+
+    size_t                           size() const;
+    int                              count( const XString& find ) const;
+
+    XString                          substr( size_t nDst ) const;
+    XString                          substr( size_t nSrc, size_t nSize ) const;
+
+    size_t                           find_last_of( XString xs ) const;
+    size_t                           find( XString xs ) const;
+
+    const wchar_t*                   c_str() const;
+
+    std::wstring                     string() { return _str; }
+
+    std::vector< XString >           split( const XString& sSplit ) const;
+
+    XString                          replaceAll( const XString& xa, const XString& xb ) const;
+    XString                          replace( const XString& xa, const XString& xb ) const;
+
+    int                              compare( const XString& xs, bool isCaseInsensitive = false ) const;
+
+    bool                             contains( const XString& xs, bool isCaseInsensitive = false ) const;
+
+    bool                             startswith( const XString& xs, bool isCaseInsensitive = false ) const;
+    bool                             endswith( const XString& xs, bool isCaseInsensitive = false ) const;
+
+protected:
+
+private:
+    std::wstring                     _str;
+    int                              _nPos = 0;
+};
+
+#endif
