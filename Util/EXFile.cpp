caf14aa888e23a20957f8596a8c6bd723a40c8d4
hogyun.yoon <alieaes54@gmail.com>
2024-03-18 19:33:30 +0900

    Add : Util, File Exist Util

---
+++ Util/EXFile.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include "EXFile.hpp"
+
+#include <filesystem>
+
+///////////////////////////////////////////////////////////////////////////
+
+bool Ext::File::IsExistFile( const XString& sFileFullPath )
+{
+    return std::filesystem::exists( sFileFullPath.toWString() );
+}
+
+bool Ext::File::IsExistDir( const XString& sDirPath )
+{
+    return std::filesystem::is_directory( sDirPath.toWString() );
+}
+
+///////////////////////////////////////////////////////////////////////////
+
---
+++ Util/EXFile.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#ifndef __HDR_EXT_UTIL_FILE__
+#define __HDR_EXT_UTIL_FILE__
+
+#include "EXString.hpp"
+
+namespace Ext
+{
+    namespace File
+    {
+        bool                                     IsExistFile( const XString& sFileFullPath );
+        bool                                     IsExistDir( const XString& sDirPath );
+    }
+}
+
+#endif
