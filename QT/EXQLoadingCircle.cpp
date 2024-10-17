3e6bc160efd13c63640d9fcc7d68c1546b23c420
hogyun.yoon <alieaes54@gmail.com>
2024-10-17 16:38:57 +0900

    Add : QtUtil, Loading Icon

---
+++ QT/EXQLoadingCircle.cpp
@@ -0,0 +1,170 @@
+﻿#include "stdafx.h"
+#include "EXQLoadingCircle.hpp"
+
+#include <QPainter>
+
+namespace Ext
+{
+    namespace QT
+    {
+        QT::qLoadingCircle::qLoadingCircle( QWidget* parent, ::Qt::WindowFlags f )
+            : QDialog( parent, f )
+        {
+            _size = QSize( 100, 100 );
+            resize( _size );
+            SetDefaultColor();
+
+            init();
+        }
+
+        qLoadingCircle::~qLoadingCircle()
+        {
+        }
+
+        void qLoadingCircle::Start()
+        {
+            if( _timer != nullptr )
+            {
+                _timer->start( _nUpdateMsec );
+                connect( _timer, &QTimer::timeout, this, &qLoadingCircle::updateAnimation );
+            }
+        }
+
+        void qLoadingCircle::Stop()
+        {
+            if( _timer != nullptr )
+            {
+                delete _timer;
+                _timer = nullptr;
+            }
+
+            _nCurrentIdx = 0;
+            _nCurrentLength = 0;
+            _isFirstRun = true;
+
+            init();
+        }
+
+        void qLoadingCircle::SetSpeed( int nSpeed )
+        {
+            _nUpdateMsec = nSpeed;
+        }
+
+        void qLoadingCircle::AddColor( const QColor& color )
+        {
+        }
+
+        void qLoadingCircle::AddGradientColor( const QGradient& gradient )
+        {
+            _vecColor.push_back( QBrush( gradient ) );
+        }
+
+        void qLoadingCircle::AddGradientAuto( const QColor& color1, const QColor& color2 )
+        {
+            int centerX = width() / 2;
+            int centerY = height() / 2;
+
+            QConicalGradient gradient;
+            gradient.setCenter( centerX, centerY );
+            gradient.setAngle( 93 );
+            gradient.setColorAt( 0, color1 );
+            gradient.setColorAt( 1, color2 );
+            _vecColor.push_back( QBrush( gradient ) );
+        }
+
+        void qLoadingCircle::ClearColor()
+        {
+            _vecColor.clear();
+        }
+
+        void qLoadingCircle::SetDefaultColor()
+        {
+            _vecColor.clear();
+
+            AddGradientAuto( "#3eccd5", "#6c8ad6" );
+            AddGradientAuto( "#6c8ad6", "#8d39d7" );
+            AddGradientAuto( "#8d39d7", "#3eccd5" );
+        }
+
+        void qLoadingCircle::SetRadius( int nRadius )
+        {
+            _nRadius = nRadius;
+        }
+
+        void qLoadingCircle::SetPenSize( int nPenSize )
+        {
+            _nPenSize = nPenSize;
+        }
+
+        void qLoadingCircle::showEvent( QShowEvent* event )
+        {
+            QDialog::showEvent( event );
+        }
+
+        void qLoadingCircle::paintEvent( QPaintEvent* event )
+        {
+            _painter->begin( this );
+            _painter->restore();
+            _painter->setRenderHint( QPainter::Antialiasing );
+
+            // 원의 중심과 반지름 설정
+            int centerX = width() / 2;
+            int centerY = height() / 2;
+            int radius = 40;
+
+            if( _isFirstRun == false )
+            {
+                int nPrevIdx = _nCurrentIdx - 1;
+                if( nPrevIdx < 0 )
+                    nPrevIdx = _vecColor.size() - 1;
+
+                // 선 색상 및 두께 설정
+                QPen pen( QBrush( _vecColor.at( nPrevIdx ) ), 5 );
+                pen.setWidth( 5 );
+                pen.setCapStyle( Qt::RoundCap );
+                _painter->setPen( pen );
+                _painter->drawArc( centerX - radius, centerY - radius, radius * 2, radius * 2, 90 * 16, -360 * 16 );
+            }
+
+
+            // 선 색상 및 두께 설정
+            QPen pen( QBrush( _vecColor.at( _nCurrentIdx ) ), 5 );
+            pen.setWidth( 5 );
+            _painter->setPen( pen );
+
+            // 원의 일부분만 그리기
+            _painter->drawArc( centerX - radius, centerY - radius, radius * 2, radius * 2, 90 * 16, -_nCurrentLength * 16 + 1 );
+
+            if( _nCurrentLength >= 360 )
+                _painter->save();
+
+            _painter->end();
+        }
+
+        void qLoadingCircle::updateAnimation()
+        {
+            if( _nCurrentLength < 360 )
+            { // 360도까지 증가
+                _nCurrentLength += 10; // 증가 속도 조정
+                update(); // 다시 그리기 요청
+            }
+            else
+            {
+                _isFirstRun = false;
+
+                if( ++_nCurrentIdx >= _vecColor.size() )
+                    _nCurrentIdx = 0;
+
+                _nCurrentLength = 0;
+                update(); // 다시 그리기 요청
+            }
+        }
+
+        void qLoadingCircle::init()
+        {
+            _timer = new QTimer( this );
+            _painter = new QPainter( this );
+            setAttribute( Qt::WA_TranslucentBackground );
+        }
+    }
+}
---
+++ QT/EXQLoadingCircle.hpp
@@ -0,0 +1,68 @@
+﻿#ifndef __HDR_QT_LOADING_CIRCLE__
+#define __HDR_QT_LOADING_CIRCLE__
+
+#include <QDialog>
+#include <QTimer>
+#include <QColor>
+#include <QWidget>
+#include "qnamespace.h"
+#include <qflags.h>
+
+namespace Ext
+{
+    namespace QT
+    {
+        class qLoadingCircle : public QDialog
+        {
+            Q_OBJECT
+
+        public:
+            qLoadingCircle( QWidget* parent = nullptr, Qt::WindowFlags f = Qt::FramelessWindowHint | Qt::Dialog );
+            virtual ~qLoadingCircle();
+
+            void                                     Start();
+            void                                     Stop();
+
+            void                                     SetSpeed( int nSpeed );
+            void                                     SetRadius( int nRadius );
+            void                                     SetPenSize( int nPenSize );
+
+            void                                     AddColor( const QColor& color );
+            void                                     AddGradientColor( const QGradient& gradient );
+            void                                     AddGradientAuto( const QColor& color1, const QColor& color2 );
+
+            void                                     ClearColor();
+
+            void                                     SetDefaultColor();
+
+        protected:
+            void                                     showEvent( QShowEvent* event ) override;
+            void                                     paintEvent( QPaintEvent* event ) override;
+
+        private slots:
+            void                                     updateAnimation();
+
+        private:
+            void                                     init();
+
+            QVector< QBrush >                        _vecColor;
+            QVector< QGradient >                     _vecGradient;
+
+            QTimer*                                  _timer             = nullptr;
+
+            QSize                                    _size;
+
+            int                                      _nCurrentLength    = 0;
+            int                                      _nCurrentIdx       = 0;
+            bool                                     _isFirstRun        = true;
+
+            int                                      _nUpdateMsec       = 25;
+            int                                      _nRadius           = 40;
+            int                                      _nPenSize          = 5;
+
+            QPainter*                                _painter;
+        };
+    }
+}
+
+#endif
