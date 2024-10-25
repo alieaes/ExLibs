9f791d16f1dc669ad07e4b7125c3c436ec6cbf5f
Alieaes <alieaes54@gmail.com>
2024-10-25 17:30:11 +0900

    add : ExFile, ExtFile Created

--- Util/EXFile.cpp
+++ Util/EXFile.cpp
@@ -5,14 +5,135 @@
 
 ///////////////////////////////////////////////////////////////////////////
 
-bool Ext::File::IsExistFile( const XString& sFileFullPath )
+namespace Ext::File
 {
-    return std::filesystem::exists( sFileFullPath.toWString() );
-}
+    bool IsExistFile( const XString& sFileFullPath )
+    {
+        return std::filesystem::exists( sFileFullPath.toWString() );
+    }
 
-bool Ext::File::IsExistDir( const XString& sDirPath )
-{
-    return std::filesystem::is_directory( sDirPath.toWString() );
+    bool IsExistDir( const XString& sDirPath )
+    {
+        return std::filesystem::is_directory( sDirPath.toWString() );
+    }
+
+    XString NormalizePath( const XString& sFullPath )
+    {
+#if defined(WIN32) || defined(_WIN32) 
+        XString sSeparator = "\\";
+        XString sSeparatorTarget = "/";
+#else 
+        XString sSeparator = "/";
+        XString sSeparatorTarget = "\\";
+#endif
+        XString sNormalizePath = sFullPath.replaceAll( sSeparatorTarget, sSeparator );
+
+        if( sNormalizePath.endswith( sSeparator ) == true )
+            sNormalizePath = sNormalizePath.substr( 0, sNormalizePath.size() - 2 );
+
+        return sNormalizePath;
+    }
+
+    cExtFile::cExtFile()
+    {
+    }
+
+    cExtFile::cExtFile( const XString& sFullPath )
+    {
+        reset();
+
+        _sFullPath = sFullPath;
+        devidePath( sFullPath );
+    }
+
+    void cExtFile::SetPath( const XString& sFullPath )
+    {
+        reset();
+
+        _sFullPath = sFullPath;
+        devidePath( sFullPath );
+    }
+
+    bool cExtFile::IsExist()
+    {
+        return std::filesystem::exists( FullPath().toWString() );
+    }
+
+    XString cExtFile::Path()
+    {
+        return _sFilePath;
+    }
+
+    XString cExtFile::Name()
+    {
+        return _sFileName;
+    }
+
+    XString cExtFile::FullPath()
+    {
+        if( _sFileName.IsEmpty() == true && _sFilePath.IsEmpty() == true )
+            return "";
+        else if( _sFileName.IsEmpty() == true )
+            return _sFilePath;
+        else
+            return _sFilePath + EXT_PATH_SEPARATOR + _sFileName;
+    }
+
+    void cExtFile::reset()
+    {
+        _isExist = false;
+        _eType = FILE_TYPE_NONE;
+
+        _sFilePath.clear();
+        _sFileName.clear();
+        _sFullPath.clear();
+    }
+
+    void cExtFile::devidePath()
+    {
+        devidePath( _sFullPath );
+    }
+
+    void cExtFile::devidePath( const XString& sFullPath )
+    {
+        XString sNormalizePath = NormalizePath( sFullPath );
+        _sFullPath = sFullPath;
+
+        if( std::filesystem::exists( sFullPath.toWString() ) == false )
+        {
+            _isExist = false;
+            return;
+        }
+
+        if( std::filesystem::is_directory( sNormalizePath.toWString() ) == true )
+        {
+            _sFilePath = sFullPath;
+            _sFileName.clear();
+            _eType = FILE_TYPE_DIR;
+        }
+        else
+        {
+            auto fnDividePath = [ this, &sNormalizePath ]() {
+                auto pos = sNormalizePath.rfind( EXT_PATH_SEPARATOR );
+                if( pos != std::string::npos )
+                {
+                    _sFilePath = sNormalizePath.substr( 0, pos - 1 );
+                    _sFileName = sNormalizePath.substr( pos, std::string::npos );
+                }
+            };
+
+            if( std::filesystem::is_regular_file( sNormalizePath.toWString() ) == true )
+            {
+                _eType = FILE_TYPE_FILE;
+                fnDividePath();
+            }
+            else if( std::filesystem::is_character_file( sNormalizePath.toWString() ) == true )
+            {
+                _eType = FILE_TYPE_CHARACTER;
+                fnDividePath();
+            }
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////
--- Util/EXFile.hpp
+++ Util/EXFile.hpp
@@ -4,6 +4,13 @@
 #define __HDR_EXT_UTIL_FILE__
 
 #include "EXString.hpp"
+#include "EXUtil.hpp"
+
+#if defined(WIN32) || defined(_WIN32) 
+    #define EXT_PATH_SEPARATOR "\\" 
+#else 
+    #define EXT_PATH_SEPARATOR "/" 
+#endif
 
 namespace Ext
 {
@@ -11,6 +18,52 @@ namespace Ext
     {
         bool                                     IsExistFile( const XString& sFileFullPath );
         bool                                     IsExistDir( const XString& sDirPath );
+        XString                                  NormalizePath( const XString& sFullPath );
+
+        enum EXT_FILE_TYPE
+        {
+            FILE_TYPE_NONE      = 0,
+            FILE_TYPE_FILE      = 1,
+            FILE_TYPE_DIR       = 2,
+            FILE_TYPE_SYMLINK   = 3,
+            FILE_TYPE_CHARACTER = 4
+        };
+
+        EXT_ENUM_START( EXT_FILE_TYPE )
+        EXT_ENUM_ADD( FILE_TYPE_NONE,      "알수없음" )
+        EXT_ENUM_ADD( FILE_TYPE_FILE,      "파일" )
+        EXT_ENUM_ADD( FILE_TYPE_DIR,       "디렉토리" )
+        EXT_ENUM_ADD( FILE_TYPE_SYMLINK,   "심볼릭 링크" )
+        EXT_ENUM_ADD( FILE_TYPE_CHARACTER, "문자 장치 파일" )
+        EXT_ENUM_END( EXT_FILE_TYPE )
+
+        class cExtFile
+        {
+        public:
+            cExtFile();
+            cExtFile( const XString& sFullPath );
+
+            void                                     SetPath( const XString& sFullPath );
+            bool                                     IsExist();
+            XString                                  Path();
+            XString                                  Name();
+            XString                                  FullPath();
+
+        private:
+            void                                     reset();
+
+            void                                     devidePath();
+            void                                     devidePath( const XString& sFullPath );
+
+        private:
+            // 존재여부는 IsExist로 확인하고, 이건 매번 확인 시 캐시값으로 남겨둠.
+            bool                                     _isExist = false;
+            EXT_FILE_TYPE                            _eType = FILE_TYPE_NONE;
+
+            XString                                  _sFullPath;
+            XString                                  _sFilePath;
+            XString                                  _sFileName;
+        };
     }
 }
 
