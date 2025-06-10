609dbc68d78e38cd926c3706ba845c1cac9da925
Alieaes <Alieaes54@gmail.com>
2025-06-11 00:54:05 +0900

    Add : ExProcess, Process Utils

---
+++ Windows/EXProcess.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+
+#include "EXProcess.hpp"
+
+bool Ext::Process::IsRunningProcess( unsigned uPID )
+{
+	bool isRunning = false;
+
+	do
+	{
+		HANDLE hProcess = OpenProcess( PROCESS_QUERY_LIMITED_INFORMATION, FALSE, uPID );
+		if( hProcess == NULL )
+			break;
+
+		DWORD dwExitCode = 0;
+
+		if( GetExitCodeProcess( hProcess, &dwExitCode ) == TRUE )
+			isRunning = ( dwExitCode == STILL_ACTIVE );
+
+		CloseHandle( hProcess );
+
+	} while( false );
+
+	return isRunning;
+}
---
+++ Windows/EXProcess.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#ifndef __HDR_EXT_PROCESS__
+#define __HDR_EXT_PROCESS__
+
+#include "EXString.hpp"
+
+namespace Ext
+{
+    namespace Process
+    {
+        bool                                     IsRunningProcess( unsigned int uPID );
+    }
+}
+#endif
